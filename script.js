3fd0da2bf78a9e774dfce1d6560435d4f8b72b7e
dinomite505 <dino.milje@gmail.com>
2023-09-25 14:20:11 +0200

    Created javascript file to order the lis alphabetically

---
+++ script.js
@@ -0,0 +1,7 @@
+// List of items
+const orderList = ["capers", "mustard", "butter", "eggs", "minced meat", "potatoes", "onions"];
+
+// Sorting the list
+orderList.sort();
+
+console.log(orderList);
